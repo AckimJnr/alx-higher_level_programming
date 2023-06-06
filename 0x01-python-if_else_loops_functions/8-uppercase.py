d0fe35791867e33e21ec6cb342481ffab05b2af9
AckimJnr <acklongwe@gmail.com>
2023-06-06 23:39:08 +0200

    Add uppercaser file

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    strlen = len(str)
+    i = 0
+
+    for char in str:
+        if i == strlen - 1:
+            endchar = "\n"
+        else:
+            endchar = ""
+        if ord(char) >= 97 and ord(char) <= 122:
+            print("{}".format(chr(ord(char) - 32)), end=endchar)
+        else:
+            print("{}".format(char), end=endchar)
+        i += 1
