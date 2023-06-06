26916765f708225c0cac3d1c13dfafccc36e49af
AckimJnr <acklongwe@gmail.com>
2023-06-07 01:21:58 +0200

    Add print tebahpaly file

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+i, j = 122, 1
+while (i > 98):
+    if j % 2 == 0:
+        char = i - 32
+    else:
+        char = i
+
+    print("{}".format(chr(char)), end="")
+    i -= 1
+    j += 1
