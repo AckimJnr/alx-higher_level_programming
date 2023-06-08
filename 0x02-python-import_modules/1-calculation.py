23209670a06d1d28c938e45d42c233ddeafb029c
AckimJnr <acklongwe@gmail.com>
2023-06-08 15:58:09 +0200

    Update calculation function

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-
+    funs = [add, sub, mul, div]
+    ops = ['+', '-', '*', '/']
     a = 10
     b = 5
-    print(a, "+", b, "=", add(a, b))
-    print(a, "-", b, "=", sub(a, b))
-    print(a, "*", b, "=", mul(a, b))
-    print(a, "/", b, "=", div(a, b))
+    i = 0
+    while (i < 5):
+        print(a, ops[i], b, "=", funs[i](a, b))
+        i += 1
