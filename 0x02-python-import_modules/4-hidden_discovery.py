2ebfc3e279233b3fb072e86e42973daca89ee593
AckimJnr <acklongwe@gmail.com>
2023-06-08 21:44:33 +0200

    Reverse engineer hiden pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@
 import marshal
 
 with open('hidden_4.pyc', 'rb') as pyc_file:
-    pyc_data = pyc_file.read();
+    pyc_data = pyc_file.read()
 
 pcfileobj = marshal.loads(pyc_data[8:])
 names = code_obj.co_names
