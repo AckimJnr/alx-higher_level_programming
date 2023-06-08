b0f0f2b0116d1ab46354cb83242ab7f46b28d2f8
AckimJnr <acklongwe@gmail.com>
2023-06-08 17:26:25 +0200

    Add infinite add file

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    argc = len(sys.argv)
+    total = 0
+
+    for i in range(1, argc):
+        total += int(sys.argv[i])
+        if i == argc - 1:
+            print("{}".format(total))
