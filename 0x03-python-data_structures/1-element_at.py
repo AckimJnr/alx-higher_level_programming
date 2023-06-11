66ca2f723d4fb59f26a7b9c09cf95f71504e7196
AckimJnr <acklongwe@gmail.com>
2023-06-11 21:38:57 +0200

    Update return statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-
     list_len = len(my_list)
 
-    if idx < 0 or idx > list_len:
-        return
+    if idx < 0 or idx >= list_len:
+        return None
 
     return my_list[idx]
