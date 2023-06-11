7c25022de31b2367422b8b1f7bba1c4271f91f34
AckimJnr <acklongwe@gmail.com>
2023-06-11 22:06:47 +0200

    Change print proces to use range

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    list_copy = my_list.copy()
-
-    list_copy.reverse()
-    for num in list_copy:
-        print("{:d}".format(num))
+    for idx in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[idx]))
