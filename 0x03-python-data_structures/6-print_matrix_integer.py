4652861019e80da262aaf731414fd2c532cf5c70
AckimJnr <acklongwe@gmail.com>
2023-06-12 21:11:01 +0200

    Update print interger matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None or len(matrix) == 0:
+        return
+    num_rows = len(matrix)
+    num_cols = len(matrix[0] if len(matrix) > 0 else 0)
+
+    if any(len(row) != num_cols for row in matrix):
+        return
+
     for row in matrix:
-        for item in row:
-            print("{:d}".format(item), end=' ')
+        for i, item in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(item), end='')
+            else:
+                print("{:d}".format(item), end=' ')
         print()
