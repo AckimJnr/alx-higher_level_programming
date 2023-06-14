3fd99a0063c6736a1330bab600d582822c4f956b
AckimJnr <acklongwe@gmail.com>
2023-06-14 22:11:45 +0200

    Update fast alphabet

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 import builtins
-print(*getattr(builtins, '__import__')('string').ascii_uppercase, sep='', end='\n')
+print(getattr(builtins, '__import__')('string').ascii_uppercase, end='\n')
