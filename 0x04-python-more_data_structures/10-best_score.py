2e965606cebb61b58eb2e2d339df06758d7b98a6
AckimJnr <acklongwe@gmail.com>
2023-06-16 17:24:35 +0200

    Add best score function

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    max_value = None
+    best_key = None
+
+    for key, value in a_dictionary.items():
+        if isinstance(value, int):
+            if max_value is None or value > max_value:
+                max_value = value
+                best_key = key
+
+    return best_key
