f055b2b53e7505047792596754a7317c08ef3966
AckimJnr <acklongwe@gmail.com>
2023-06-26 09:34:33 -0700

    Add error check

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
-        print("{} is not an interger".format(value))
+    except:
         return False
