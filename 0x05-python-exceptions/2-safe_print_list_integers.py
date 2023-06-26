3d12d713dff8cdc7e04b926f7260085b2f9972a4
AckimJnr <acklongwe@gmail.com>
2023-06-26 09:57:06 -0700

    Raise error in file

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for item in my_list[:x]:
-            try:
-                print("{:d}".format(item), end='')
-                count += 1
-            except ValueError:
-                pass
-    except TypeError:
-        return count
-    finally:
-        print()
-        return count
+    for item in my_list[:x]:
+        try:
+            print("{:d}".format(item), end='')
+            count += 1
+        except ValueError:
+            pass
+    print()
+    return count
