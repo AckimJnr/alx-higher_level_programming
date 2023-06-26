40071f622ca3f00b903b09ce6606a7bebca835d8
AckimJnr <acklongwe@gmail.com>
2023-06-26 09:58:46 -0700

    Print lis intergers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for item in my_list[:x]:
-        try:
-            print("{:d}".format(item), end='')
-            count += 1
-        except ValueError:
-            pass
-    print()
-    return count
+    try:
+        for item in my_list[:x]:
+            try:
+                print("{:d}".format(item), end='')
+                count += 1
+            except ValueError:
+                pass
+    except TypeError:
+        return count
+    finally:
+        print()
+        return count
