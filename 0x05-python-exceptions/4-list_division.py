e2c3dd0ce0e49c0ee36cd1f48ac3c1a9b9d6a51c
AckimJnr <acklongwe@gmail.com>
2023-06-28 16:59:02 +0200

    Add finaly block

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,11 +8,11 @@ def list_division(my_list_1, my_list_2, list_length):
             new_list.append(result)
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
         except TypeError:
             print("wrong type")
-            new_list.append(0)
         except IndexError:
             print("out of range")
+        finally:
             new_list.append(0)
+            
     return new_list
