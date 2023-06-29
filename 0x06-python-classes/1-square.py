81a5e36e850fc9dd2ac7f5f24b0faf1facaa1943
AckimJnr <acklongwe@gmail.com>
2023-06-29 17:47:00 +0200

    Add 1 square file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" This class defines a square """
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new instance of a square class
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
