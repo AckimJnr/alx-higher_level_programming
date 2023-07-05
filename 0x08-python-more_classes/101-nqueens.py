49e278542891d058103ed9e6781d68b06cc980e5
AckimJnr <acklongwe@gmail.com>
2023-07-06 00:50:28 +0200

    Update nqueens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Implementantion of the nqueens puzzle """
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
