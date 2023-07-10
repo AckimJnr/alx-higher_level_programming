dcb7bff5b8b770de54294861a12286ef2fed1d5d
AckimJnr <acklongwe@gmail.com>
2023-07-10 22:04:58 +0200

    Add lookup file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-
+""" Module that works on objects """
 
 def lookup(obj):
+    """
+    Returns a list of attributes and methods of an object
+
+    Args:
+        obj: The object to inspect
+    Returns:
+        a list of available attributes and methods of the object
+    """
     return dir(obj)
