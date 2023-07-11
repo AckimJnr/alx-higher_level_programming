f58e3b02dfaa7ddbff06e475f6ea9eb3ebe62d80
AckimJnr <acklongwe@gmail.com>
2023-07-11 15:22:57 +0200

    Update requirement

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""module similiarities between object instance and class"""
 
 
 def is_kind_of_class(obj, a_class):
-    """ Returns True if an object is the instance of """
+    """Returns True if an object is the instance of"""
     if isinstance(obj, a_class):
         return True
     else:
