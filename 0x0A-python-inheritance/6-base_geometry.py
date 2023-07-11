a09d0f1885f0695b11df356fd8b179aad76d402f
AckimJnr <acklongwe@gmail.com>
2023-07-11 19:37:24 +0200

    Add geometry base

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Geometry module extension"""
+
+
+class BaseGeometry:
+    """Contains operations to work on geometry"""
+
+    def area(self):
+        """Area Calculation"""
+        raise Exception("area() is not implemented")
