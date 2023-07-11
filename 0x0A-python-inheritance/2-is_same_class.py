e8f277433cc1be6fa5c8f02c05c66399090be5b7
AckimJnr <acklongwe@gmail.com>
2023-07-11 15:19:45 +0200

    Add same as class and is kind of class module

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def is_same_class(obj, a_class):
+    """Checks if an object is of an instance of the given class"""
+    return type(obj) is a_class
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def is_kind_of_class(obj, a_class):
+    """ Returns True if an object is the instance of """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        if isinstance(obj, type):
+            return is_kind_of_class(obj, a_class.__bases__)
+        else:
+            return False
