f3f3cc960341d59754da8b80558f54f8a8f7ec6f
AckimJnr <acklongwe@gmail.com>
2023-07-13 04:38:13 +0200

    Add append

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""append string to doc"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Append new data"""
+    with open(filename, "r+") as doc:
+        lines = doc.readlines()
+        doc.seek(0)
+
+        for line in lines:
+            doc.write(line)
+            if seach_string in line:
+                doc.write(new_string + "\n")
+        doc.truncate()
