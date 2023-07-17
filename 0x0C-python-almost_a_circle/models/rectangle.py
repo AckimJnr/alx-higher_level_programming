28d8d4e3bcc519f39f41e3305195f7a29fd5ca23
AckimJnr <acklongwe@gmail.com>
2023-07-17 22:15:13 +0200

    Handle empty width and height

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,7 +26,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if value is None or not isinstance(value, int):
             raise TypeError("width must be an interger")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -38,7 +38,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if value is None or not isinstance(value, int):
             raise TypeError("height must be an interger")
         if value <= 0:
             raise ValueError("height must be > 0")
