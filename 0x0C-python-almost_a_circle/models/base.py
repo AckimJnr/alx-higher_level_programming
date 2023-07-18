2bca97e29b2ed4cbf02fbc9c2181f627b9c526af
AckimJnr <acklongwe@gmail.com>
2023-07-19 01:25:44 +0200

    Update base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,3 +43,12 @@ class Base:
                 )
         with open(filename, 'w') as file:
             file.write(j_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
