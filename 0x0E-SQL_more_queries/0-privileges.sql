6b0450e38b3edc7cc5294f12e7237f864dea2613
AckimJnr <acklongwe@gmail.com>
2023-08-16 20:41:23 +0200

    update host

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- show user previledges
 -- for two users in the database
-SHOW GRANTS FOR user_0d_1;
-SHOW GRANTS FOR user_0d_2;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
