f1d45e965496257e4a18136d067a3148b1049330
AckimJnr <acklongwe@gmail.com>
2023-09-12 15:11:50 +0200

    Add file

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-const argLength = process.argv.length;
-
-if (argLength > 2) {
+if (process.argv.length > 2) {
   console.log(process.argv[2]);
 } else {
   console.log('No argument');
