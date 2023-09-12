c54c78ff11df5ff808cc0fe0b51ed752d1ddf3f2
AckimJnr <acklongwe@gmail.com>
2023-09-12 16:46:34 +0200

    Update semi standard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
-if (process.argv.length < 3) {
-  console.log('Missing number of occurrences');
+const input = process.argv[2];
+
+const integerValue = parseInt(input);
+
+if (!isNaN(integerValue)) {
+  for (let i = 0; i < integerValue; i++) console.log('C is fun');
 } else {
-  const input = process.argv[2];
-
-  const integerValue = parseInt(input);
-
-  if (!isNaN(integerValue)) {
-    for (let i = 0; i < integerValue; i++) console.log('C is fun');
-  } else {
-    console.log('Not a number');
-  }
+  console.log('Missing number of occurrences');
 }
