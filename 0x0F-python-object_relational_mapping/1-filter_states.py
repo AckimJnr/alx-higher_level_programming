d97d94d992dd01b1524399bdfab8b06435bf3051
AckimJnr <acklongwe@gmail.com>
2023-09-16 13:18:43 +0200

    Update filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,24 +13,32 @@ if __name__ == '__main__':
     """
     This block makes it execute only as the main module
     """
-    dbhost = "localhost"
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
+    try:
+        dbhost = "localhost"
+        username = sys.argv[1]
+        password = sys.argv[2]
+        dbname = sys.argv[3]
 
-    conn = MySQLdb.connect(
-            host=dbhost,
-            port=3306,
-            user=username,
-            passwd=password,
-            db=dbname,
-            charset="utf8")
+        conn = MySQLdb.connect(
+                host=dbhost,
+                port=3306,
+                user=username,
+                passwd=password,
+                db=dbname,
+                charset="utf8")
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states\
+                    WHERE name LIKE 'N%'\
+                    ORDER BY id ASC")
 
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+
+    except mysl.connector.Error as err:
+        print("Error: {}".format(err))
+
+    finally:
+        cur.close()
+        conn.close()
