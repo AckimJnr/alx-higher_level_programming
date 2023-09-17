0eb946c819ee6f699c38a7e738f6f193c0189351
AckimJnr <acklongwe@gmail.com>
2023-09-17 21:50:39 +0200

    Add 9-model

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Lists one first state
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Lets this execute only when not imported
+    """
+    dbUsername = sys.argv[1]
+    dbPassword = sys.argv[2]
+    dbName = sys.argv[3]
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                dbUsername,
+                dbPassword,
+                dbName),
+            pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter(
+            State.name.like(f'%a')).order_by(State.id)
+    if query is None:
+        print("Nothing")
+    else:
+        for state in query:
+            print("{}: {}".format(state.id, state.name))
+
+    session.close()
