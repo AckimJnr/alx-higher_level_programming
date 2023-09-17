14c7f32ca28e7a4fd87ffefb469d9cf2b189e283
AckimJnr <acklongwe@gmail.com>
2023-09-17 20:27:51 +0200

    Add query one state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Lists one first state
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Lets this execute only when not imported
+    """
+    dbUsername = sys.argv[1]
+    dbPassword = sys.argv[2]
+    dbName = sys.argv[3]
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                dbUsername,
+                dbPassword,
+                dbName),
+            pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).order_by(State.id).first()
+
+    print("{}: {}".format(query.id, query.name))
+
+    session.close()
