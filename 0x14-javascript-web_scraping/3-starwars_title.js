2b6b1ad21927e7c3f4ca3b7d41749dec8e7eba74
AckimJnr <acklongwe@gmail.com>
2023-10-24 20:35:06 +0200

    update get query

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,17 +2,11 @@
 // make s a GET reqiest to a url
 const request = require('request');
 
-const baseUrl = 'https://swapi-api.alx-tools.com/api/films/?id=';
+const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
 const movieId = Number(process.argv[2]);
 const url = baseUrl + movieId;
-
 request(url, (error, response, body) => {
   if (error) console.log(error);
-  const movies = JSON.parse(body);
-
-  for (const movie of movies.results) {
-    if (movie.episode_id === movieId) {
-      console.log(movie.title);
-    }
-  }
+  const movie = JSON.parse(body);
+  console.log(movie.title);
 });
