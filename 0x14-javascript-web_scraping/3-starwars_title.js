055387bca1cf1c4235dacb4223e2d3ffcbce9ec8
AckimJnr <acklongwe@gmail.com>
2023-10-24 19:52:00 +0200

    Update json parsing

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,9 +2,12 @@
 // make s a GET reqiest to a url
 const request = require('request');
 
-const baseUrl = "https://swapi-api.alx-tools.com/api/films/?id=";       
+const baseUrl = 'https://swapi-api.alx-tools.com/api/films/?id=';
+const movieId = process.argv[2];
+const url = baseUrl + movieId;
+
 request(url, (error, response, body) => {
   if (error) console.log(error);
-  console.log('code: ' + response.statusCode);
-        console.log(body);
+  const movieData = JSON.parse(body);
+  console.log(movieData.results[0].title);
 });
