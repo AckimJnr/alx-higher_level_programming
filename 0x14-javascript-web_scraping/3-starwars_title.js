0f0101eb6eef4c4e19d6a1ae66ba0b1b5864e78e
AckimJnr <acklongwe@gmail.com>
2023-10-24 20:25:06 +0200

    Update semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,16 @@
 const request = require('request');
 
 const baseUrl = 'https://swapi-api.alx-tools.com/api/films/?id=';
-const movieId = process.argv[2];
+const movieId = Number(process.argv[2]);
 const url = baseUrl + movieId;
 
 request(url, (error, response, body) => {
   if (error) console.log(error);
-  const movieData = JSON.parse(body);
-  console.log(movieData.results[0].title);
+  const movies = JSON.parse(body);
+
+  for (const movie of movies.results) {
+    if (movie.episode_id === movieId) {
+      console.log(movie.title);
+    }
+  }
 });
