bce1fa72a32de2ab2f0434f935921b493bb41358
AckimJnr <acklongwe@gmail.com>
2023-10-24 10:38:28 -0700

    Add start wars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// make s a GET reqiest to a url
+const request = require('request');
+
+const baseUrl = "https://swapi-api.alx-tools.com/api/films/?id=";
+
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  console.log('code: ' + response.statusCode);
+	console.log(body);
+});
