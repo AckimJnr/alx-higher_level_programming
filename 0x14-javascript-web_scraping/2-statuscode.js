253833ad85993068c435c785abf37ff1ac1c6f81
AckimJnr <acklongwe@gmail.com>
2023-10-24 16:32:26 +0200

    Update status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,12 +2,9 @@
 // make s a GET reqiest to a url
 const request = require('request');
 
-if (process.argv >= 3) {
-  const url = process.argv[2];
+const url = process.argv[2];
 
-  request(url, (error, response, body) => {
-    if (error) console.log(error);
-
-    console.log('code: ' + response.statusCode);
-  });
-}
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  console.log('code: ' + response.statusCode);
+});
