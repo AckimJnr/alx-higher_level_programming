e4f72d083ee8b291510779d34c308f0f58b7b385
AckimJnr <acklongwe@gmail.com>
2023-10-25 13:56:25 +0200

    Update 2 square file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 
+
 class Square:
-"""A Class defining a square and its ops """
+    """A Class defining a square and its ops """
 
+    def __init__(self, size=0):
+        """
+        Constructor to initialize the square values
 
-	def __init__(self, size=0):
-		"""
-		Constructor to initilize the square values
+        Args:
+            size (int, optional): The size of the square
 
-		Args:
-			size (int, optional): The size of the square
-
-		Raises:
-			TypeError: If size is not an interger
-			Value Error: if size is less than 0
-		"""
-		if not isinstance(size, int):
-			raise TypeError("size must be an interger")
-		elif size < 0:
-		 	raise ValueError("size must be >= 0")
-		 else:
-		 	self.__size = size
-
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
